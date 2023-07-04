585a367f5c0d1da4a3edd8e4901cc6e24a3d424e
Harshpathakjnp <92443560+Harshpathakjnp@users.noreply.github.com>
2023-07-04 15:02:22 +0530

    added API Calling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
+import React, { useEffect, useState } from 'react';
 
+import logo from './logo.svg';
 import './App.css';
-import VsjStyle from './components/VsjStyle';
+
 
 
 
 //
-
+let city = "varanasi";
 function App(props) {
 
-  return (
+
+  const [weather, setWeather] = useState([{ name: "", main: [] }]);
+  const [mn, setMain] = useState([{ main: [] }]);
+  
+  async function getWeatherHandler() {
+    // const city="ghazipur";
+    const appid = "4a1f8a61b74546825af1e0be106e797b";
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appid + "&units=metric");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setWeather(data);
+    setMain(data.main);
+
+  }
+  useEffect(() => {
+    //getWeatherHandler();
+
+  }
+
+  );
+
+  const handleChange = event => {
+    //setWeather(event.target.value);
+    city = event.target.value;
+    console.log('value is:', event.target.value);
+  };
+
+
+
+  return (<>
+<center>
+<div className='center_div'>
+  <div className='main_div'>
     <div className="App">
-      <h2>from VsjStyle</h2>
-      <VsjStyle />
+
+      <input type="text"
+        placeholder="city"
+        id="weather"
+        name="weather"
+        onChange={handleChange}
+      />
 
 
+      <button type='button' onClick={getWeatherHandler}>Search</button>
+      <h2>weather {weather.name}</h2>
+      <h2>weather {mn.temp}</h2>
+
+
+
+      </div>
+      </div>
     </div>
+    </center>
+  </>
   );
 }
 
