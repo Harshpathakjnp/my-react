cecafd527ec2bc82f8fc437058f2f650f36c1238
Harshpathakjnp <92443560+Harshpathakjnp@users.noreply.github.com>
2023-07-05 10:52:07 +0530

    helloEffect Added

--- src/App.js
+++ src/App.js
@@ -1,70 +1,13 @@
-import React, { useEffect, useState } from 'react';
-
 import logo from './logo.svg';
 import './App.css';
+import HelloEffect from './components/HelloEffect';
 
-
-
-
-//
-let city = "varanasi";
-function App(props) {
-
-
-  const [weather, setWeather] = useState([{ name: "", main: [] }]);
-  const [mn, setMain] = useState([{ main: [] }]);
-  
-  async function getWeatherHandler() {
-    // const city="ghazipur";
-    const appid = "4a1f8a61b74546825af1e0be106e797b";
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appid + "&units=metric");
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    setWeather(data);
-    setMain(data.main);
-
-  }
-  useEffect(() => {
-    //getWeatherHandler();
-
-  }
-
-  );
-
-  const handleChange = event => {
-    //setWeather(event.target.value);
-    city = event.target.value;
-    console.log('value is:', event.target.value);
-  };
-
-
-
-  return (<>
-<center>
-<div className='center_div'>
-  <div className='main_div'>
+function App() {
+  return (
     <div className="App">
-
-      <input type="text"
-        placeholder="city"
-        id="weather"
-        name="weather"
-        onChange={handleChange}
-      />
-
-
-      <button type='button' onClick={getWeatherHandler}>Search</button>
-      <h2>weather {weather.name}</h2>
-      <h2>weather {mn.temp}</h2>
-
-
-
-      </div>
-      </div>
+      <h1>Varanasi Software Junction</h1>
+      <HelloEffect/>      
     </div>
-    </center>
-  </>
   );
 }
 
--- src/components/HelloEffect.js
+++ src/components/HelloEffect.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 import axios from "axios";
-import './HelloEffect.css';
 
 function HelloEffect() {
     const [people, setPeople] = useState(1);
@@ -10,49 +9,47 @@ function HelloEffect() {
     const [films, setFilms] = useState(null);
     useEffect(() => {
         axios.get(baseURL).then((response) => {
-console.log(response.data.films);
+            console.log(response.data.films);
             setStar(response.data);
             setFilms(response.data.films);
         }).catch(error => {
-            setStar({"name":"","mass":""});
+            setStar({ "name": "", "mass": "" });
             setFilms(['No Data']);
             //setError(error);
-          });
+        });
     }, [people]);
     const peopleChange = (event) => {
-        let n=event.target.value;
-       n=n.trim();
-       if(n=="")
-       n="0";
-       n=Number(n);
+        let n = event.target.value;
+        n = n.trim();
+        if (n == "")
+            n = "0";
+        n = Number(n);
         setPeople(n);
         console.log(event.target.value);
     }
     if (!star) return (<div>
-        <center> 
-            <input type="text"    onChange={peopleChange}/>
+        <center>
+            <input type="text" onChange={peopleChange} />
             <p>No Data</p>
 
         </center>
     </div>);
 
 
-        return <div>
-        <center> 
-            <input type="text" value={people}  onChange={peopleChange}/>
-            <p>Name {(star.name)?star.name:"No Data"}</p>
-            <p>Mass {(star.mass)?star.mass:"No Data"}</p>
-            
+    return <div>
+        <center>
+            <input type="text" value={people} onChange={peopleChange} />
+            <p>Name {(star.name) ? star.name : "No Data"}</p>
+            <p>Mass {(star.mass) ? star.mass : "No Data"}</p>
+
             {films.map(current => {
-                    return (
-                        <ul className='liwidth' key={current}>
-                            <li> <a target={"_blank"} href={current}>{current}</a>
-
-
-                            </li>
-                        </ul>
-                    );
-                })}
+                return (
+                    <ul className='liwidth' key={current}>
+                        <li> <a target={"_blank"} href={current}>{current}</a>
+                        </li>
+                    </ul>
+                );
+            })}
 
 
         </center>
