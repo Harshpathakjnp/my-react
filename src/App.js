10ee0e1ccac8c23d714f32f126dca9a9cce25bc5
Harshpathakjnp <92443560+Harshpathakjnp@users.noreply.github.com>
2023-07-13 11:19:50 +0530

    imageccess added using json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import ImageAccess from './components/ImageAccess';
 function App() {
   return (
     <div className="App">
+      <ImageAccess/>
       
 
     </div>
--- src/components/ImageAccess.js
+++ src/components/ImageAccess.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { useState,useEffect } from 'react';
-import axious from 'axious';
+import axios from 'axios';
 
 function ImageAccess() {
-    const [album,setAlbum] = useState([]);
+    const [album,setAlbum] = useState(null);
     const [image,setImage] = useState('');
+    const baseURL = `https://gist.githubusercontent.com/champaksworldcreate/b5314c76cc15b44686c7151a8fb039b0/raw/d6020258eea665ef2929f901d88ae3d41e22ba6b/images.json`;
 
     useEffect(()=>{
-        axious.get().then((response) => {
-            console.log(response.data);
-            setAlbum(response.data);
+        axios.get(baseURL).then((response) => {
+            let albumdata = response.data;
+            console.log(albumdata[0].url);
+            setAlbum(albumdata);
         }).catch((err) => {
-            <p>Error</p>
+            setAlbum({ "albumname": "","sno": "","name": "", "url": "" });
             
         });
-    })
+    },[])
+    if (!album) return (<div>
+        <center>
+            <p>No Data</p>
+        </center>
+    </div>);
   return (
     <div>
-        <p>{album[0].url}</p>
+        <h1> 
+            {album.map((current,i)=>{
+                return <div key={i}>
+                    <img src={current.url} alt={current.name}></img>
+                </div>
+            })}
+        </h1>
       
     </div>
   )
